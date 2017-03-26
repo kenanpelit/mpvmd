19929bf3c0f1b6a22af2394443b336fe54803cdb
rr- <rr-@sakuya.pl>
2017-03-26 17:34:33 +0200

    build: add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='Marcin Kurczewski',
+    author_email='rr-@sakuya.pl',
+    name='mpvmd',
+    long_description='mpv music daemon',
+    version='0.1',
+    url='https://github.com/rr-/mpvmd',
+    packages=find_packages(),
+
+    entry_points={
+        'console_scripts': [
+            'mpvmd = mpvmd.server.__main__:main',
+            'mpvmc = mpvmd.client.__main__:main'
+        ]
+    },
+
+    install_requires=[
+        'mpv',
+        'parsimonious',
+    ],
+
+    classifiers=[
+        'Environment :: Console',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Desktop Environment',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ])
